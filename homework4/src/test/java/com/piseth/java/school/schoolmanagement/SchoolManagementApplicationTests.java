298dbff4fe492d8d4ba8eab07fb36b4b20557ac8
Piseth Ing <piseth.ing@allianz.com>
2022-10-03 11:17:25 +0700

    init project

---
+++ homework4/src/test/java/com/piseth/java/school/schoolmanagement/SchoolManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piseth.java.school.schoolmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
