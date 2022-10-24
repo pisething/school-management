44195480e4c15c822c70b28e154757ad118d662e
DEVE-SOPHAK <sophak1502@gmail.com>
2022-10-24 15:24:18 +0700

    init project

---
+++ student-mgmt/src/main/java/com/sophak/studentmgmt/StudentMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.sophak.studentmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMgmtApplication.class, args);
+	}
+
+}
---
+++ student-mgmt/src/test/java/com/sophak/studentmgmt/StudentMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sophak.studentmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
