a7a594730a357c7edf60f370aa2ff0ff7c559545
Piseth Ing <piseth.ing@allianz.com>
2022-10-25 16:43:04 +0700

    CRUD for students

---
+++ src/main/java/com/piseth/java/school/schoolManagement/SchoolManagementPisethApplication.java
@@ -0,0 +1,13 @@
+package com.piseth.java.school.schoolManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementPisethApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagementPisethApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/piseth/java/school/schoolManagement/mapper/StudentMapper.java
@@ -0,0 +1,17 @@
+package com.piseth.java.school.schoolManagement.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import com.piseth.java.school.schoolManagement.dto.StudentDTO;
+import com.piseth.java.school.schoolManagement.model.Student;
+
+@Mapper
+public interface StudentMapper {
+	StudentMapper INSTANCE = Mappers.getMapper(StudentMapper.class);
+	
+	Student toEntity(StudentDTO dto);
+	
+	StudentDTO toDTO(Student entity);
+
+}
