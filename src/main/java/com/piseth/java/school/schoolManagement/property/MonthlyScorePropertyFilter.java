aac3e258a62be08d1782111d136461c191658249
kimhakSeang <104565674+kimhakSeang@users.noreply.github.com>
2022-12-29 15:51:07 +0700

    Revert "Revert "List Scores : Updated""
    This reverts commit 9f1bdf1296cb5b0476d43ec4d4a3f4c6cba64070.

--- src/main/java/com/piseth/java/school/schoolManagement/property/MonthlyScorePropertyFilter.java
+++ src/main/java/com/piseth/java/school/schoolManagement/property/MonthlyScorePropertyFilter.java
@@ -1,6 +1,7 @@
 package com.piseth.java.school.schoolManagement.property;
 
 public interface MonthlyScorePropertyFilter {
+	String STUDENTID="studentId";
 	String GRADE = "grade";
 	String CLASS_NAME = "className";
 	String YEAR = "year";
